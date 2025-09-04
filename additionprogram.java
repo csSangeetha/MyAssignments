46ac09ffdc9b8320891b3381710afd4d03f79b96
Sangeetha <102424272+csSangeetha@users.noreply.github.com>
2025-09-05 11:42:25 +1200

    Guru Kumaraa Exercise
    Completed the exercise

---
+++ additionprogram.java
@@ -0,0 +1,17 @@
+
+public class additionprogram 
+{
+	public static void main(String[] args) 
+	{
+		
+	System.out.println("Hello Guru Kumaraa");	
+	System.out.println(40+60);
+		
+		
+		
+
+	}
+
+}
+
+
